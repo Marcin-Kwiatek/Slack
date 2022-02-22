208bcc982cbf0d8cdbf547422de53b3babecbcb2
Marcin <kwiatekm4@gmail.com>
2022-02-22 17:17:11 +0100

    show error if login or password is empty

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from './Logo';
 import '../styles/signInAndSignUp.css'
 import { Link, useHistory } from "react-router-dom";
@@ -7,18 +7,34 @@ import { Link, useHistory } from "react-router-dom";
 
 function SignIn() {
 
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+    const [err, setErr] = useState('');
+
     const history = useHistory()
 
     const Login = () => {
-        history.push('/')
+        if (login === "" || password === "") {
+            setErr('Nie poprawna wartość login lub hasło')
+        }
+        else {
+            history.push('/')
+        }
+    }
+    const changeLogin = (e) => {
+        setLogin(e.target.value)
+    }
+    const changePassword = (e) => {
+        setPassword(e.target.value)
     }
 
     return (
         <div className="container">
             <Logo></Logo>
-            <input placeholder="Login" className="input" maxLength="20" type="text"></input>
-            <input placeholder="Hasło" className="input" maxLength="20" type="password"></input>
+            <input placeholder="Login" className="input" maxLength="20" type="text" onChange={changeLogin}></input>
+            <input placeholder="Hasło" className="input" maxLength="20" type="password" onChange={changePassword}></input>
             <button className="submit_button" onClick={Login}>Zaloguj się</button>
+            <div className="err">{err}</div>
             <div className="text">Nie masz konta? <Link className='link' to='/SignUp'>Zarejestruj się</Link></div>
         </div>
     )
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from './Logo';
 import { Link, useHistory } from "react-router-dom";
 import '../styles/signInAndSignUp.css'
@@ -7,18 +7,34 @@ import '../styles/signInAndSignUp.css'
 
 function SignUp() {
 
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+    const [err, setErr] = useState('');
+
     const history = useHistory()
 
     const SignUp = () => {
-        history.push("/SignIn");
+        if (login === "" || password === "") {
+            setErr('Nie poprawna wartość login lub hasło')
+        }
+        else {
+            history.push("/SignIn");
+        }
+    }
+    const changeLogin = (e) => {
+        setLogin(e.target.value)
+    }
+    const changePassword = (e) => {
+        setPassword(e.target.value)
     }
 
     return (
         <div className="container">
             <Logo></Logo>
-            <input placeholder="Login" className="input" maxLength="20" type="text"></input>
-            <input placeholder="Hasło" className="input" maxLength="20" type="password"></input>
+            <input placeholder="Login" className="input" maxLength="20" type="text" onChange={changeLogin}></input>
+            <input placeholder="Hasło" className="input" maxLength="20" type="password" onChange={changePassword}></input>
             <button className="submit_button" onClick={SignUp}>Zarejestruj się</button>
+            <div className="err">{err}</div>
             <div className="text">Masz już konto? <Link className='link' to='/SignIn'>Zaloguj się</Link></div>
         </div>
     )
