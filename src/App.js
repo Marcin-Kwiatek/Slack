21d3cbdc340cfd25d4e39ac26cc355f0eb803c85
Marcin <kwiatekm4@gmail.com>
2022-02-22 16:00:00 +0100

    add react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
 import React from 'react';
 import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 
 
 function App() {
 
     return (
-        <SignIn></SignIn>
-    )
+        <Router>
+            <Switch>
+                <Route exact path='/SignIn'>
+                    <SignIn></SignIn>
+                </Route>
+                <Route exact path='/SignUp'>
+                    <SignUp></SignUp>
+                </Route>
+            </Switch>
+        </Router>)
 }
 
 export default App;
